a77099e4eea94bcad7dce9f5f4cb3179679cc3b1
ouou-haha <ou471072445@163.com>
2025-06-17 09:43:54 +0800

    update save tsr to txt

--- src/spu_host_cal/data_tool.py
+++ src/spu_host_cal/data_tool.py
@@ -233,14 +233,17 @@ def generate_matrix(w: int, c: int, datatype: torch.dtype) -> torch.Tensor:
         return torch.randn(w, c).to(datatype)
 
 
-def save_tensor_as_decimal_txt(input_tensor: torch.Tensor, txt_file: str) -> None:
+def save_tensor_as_decimal_txt(input_tensor: torch.Tensor, txt_file: str, keep_num: bool = False) -> None:
     input_tensor = input_tensor.reshape(1, -1)
     with open(txt_file, 'w') as f:
         for row in input_tensor:
             if input_tensor.dtype in (torch.int8, torch.int32):
                 line = " ".join([str(int(x)) for x in row])
             else:
-                line = " ".join([str(float(x)) for x in row])
+                if keep_num is False:
+                    line = " ".join([str(float(x)) for x in row])
+                else:
+                    line = " ".join([f"{float(x):.7f}" for x in row])
             f.write(line + " ")
 
 
