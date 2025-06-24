997148767b5c4e699d7ae4762fae77f51298f751
ouou-haha <ou471072445@163.com>
2025-06-24 10:26:06 +0800

    update topk

--- src/spu_host_cal/data_tool.py
+++ src/spu_host_cal/data_tool.py
@@ -434,8 +434,7 @@ def get_topk_index(bank_vec: torch.Tensor, k: int, flag: bool = True) -> Tuple[
     combined_sorted = sorted(combined, key=lambda bank_vec: (-bank_vec[0], bank_vec[1]))
 
     # flag == False 两个数绝对值相等时优先index较小  True优先较大的
-    topk_indices = torch.tensor([idx for _, idx in combined_sorted[:k]]) if flag \
-        else torch.sort(abs_bank_vec)[1][48:]
+    topk_indices = torch.tensor([idx for _, idx in combined_sorted[:k]])
     sorted_topk_indices, _ = torch.sort(topk_indices)
     return sorted_topk_indices, topk_indices, bank_vec[topk_indices]
 
